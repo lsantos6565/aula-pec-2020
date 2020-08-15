039b58860bf8a6d134e6be96bab004050f793770
Lucas Santos <58743608+lsantos6565@users.noreply.github.com>
2020-08-15 09:33:51 -0300

    Criei o arquivo segunda.py

---
+++ segunda.py
@@ -0,0 +1,7 @@
+primeiroNumero = int(input('Digite um número: '))
+segundoNumero = int(input('Digite outro número: '))
+terceiroNumero = int(input('Digite mais um número: '))
+
+media = (primeiroNumero + segundoNumero + terceiroNumero)/3
+
+print(f'A média entre {primeiroNumero}, {segundoNumero} e {terceiroNumero} é igual a {media}.')
